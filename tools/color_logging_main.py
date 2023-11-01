ab0cc7126f986636247914f2d1c4ef09ee063ec5
Kiyoon Kim <yoonkr33@gmail.com>
2023-11-01 20:51:30 +0900

    feat: use rich logger over coloredlogs

--- tools/color_logging_main.py
+++ tools/color_logging_main.py
@@ -5,16 +5,11 @@ install()
 import logging
 import os
 
-import coloredlogs
-import verboselogs
+from rich.logging import RichHandler
 
 import zproject
 
-# Use the standard logging module for most of the modules
-# logger = logging.getLogger(__name__)
-
-# Use the VerboseLogger in the main module, so that we can use logger.success
-logger = verboselogs.VerboseLogger(__name__)  # add logger.success
+logger = logging.getLogger(__name__)
 
 log_path = "train.log"
 
@@ -31,17 +26,23 @@ if __name__ == "__main__":
     If we set the level during the initialisation, it will affect to ALL streams,
     so the file stream cannot be more verbose (lower level) than the console stream.
     """
-    # logging.basicConfig(format='', level=logging.NOTSET, stream=open(os.devnull, 'w'))
-    coloredlogs.install(fmt="", level=logging.NOTSET, stream=open(os.devnull, "w"))
+    logging.basicConfig(
+        format='',
+        datefmt="%Y/%m/%d %H:%M:%S",
+        level=logging.NOTSET,
+        stream=open(os.devnull, 'w'),
+    )
 
     # If you want to suppress logs from other modules, set their level to WARNING or higher
     # logging.getLogger('slowfast.utils.checkpoint').setLevel(logging.WARNING)
 
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.INFO)
-    console_format = coloredlogs.ColoredFormatter(
-        "%(name)s: %(lineno)4d - %(levelname)s - %(message)s"
+    console_handler = RichHandler(
+        level=logging.INFO,
+        show_time=True,
+        show_level=True,
+        show_path=True,
     )
+    console_format = logging.Formatter("%(name)s - %(message)s")
     console_handler.setFormatter(console_format)
 
     f_handler = logging.FileHandler(log_path)
@@ -59,6 +60,6 @@ if __name__ == "__main__":
     try:
         logger.info(f"zproject {zproject.__version__}")
         main()
-        logger.success("Successfully completed!")
+        logger.info("💖 Successfully completed!")
     except Exception:
         logger.exception("Exception occurred")
